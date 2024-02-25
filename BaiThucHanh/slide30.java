36a12254ec78118cc663bcb1af99a52db08f9081
unknown <phamanhnguyenhung@gmail.com>
2024-02-25 12:06:07 +0700

    slide30

---
+++ BaiThucHanh/slide30.java
@@ -0,0 +1,17 @@
+package BaiThucHanh;
+
+import java.util.Scanner;
+
+public class slide30 {
+    public static void main(String[] args) {
+        int n,sum=0,du;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("nhap n:");n=sc.nextInt();
+        while(n>0){
+            du= n % 10;
+            n= n / 10;
+           sum+= du;
+        }
+        System.out.print("tong la:"+ sum );
+    }
+}
