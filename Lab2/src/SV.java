fd09e26a955200782ebd10e1c4fa047186d7e564
unknown <phamanhnguyenhung@gmail.com>
2024-05-01 16:11:52 +0700

    interface

--- Lab2/src/SV.java
+++
@@ -1,9 +0,0 @@
-package BaiThucHanh;
-
-public class SV {
-    public static void main(String[] args) {
-    mainSv a = new mainSv() ;
-    a.NhapThongTin();
-    a.XuatThongTin();
-}
-}
--- Lab2/src/main.java
+++
@@ -1,9 +0,0 @@
-package BaiThucHanh;
-
-public class main {
-    public static void main(String[] args) {
-     Test a = new Test();
-    a.EnterDate();
-    a.Display();
-    }
-}
